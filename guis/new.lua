5bbd2f7a97e62b6093ab5a3c85275df0c2cdbfcd
wrealaero <clanwarobi@gmail.com>
2026-09-08 20:51:33 -0500

    Update new.lua

--- guis/new.lua
+++ guis/new.lua
@@ -1,5 +1,6 @@
 --This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.
 --This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.
+--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.
 
 local mainapi = {
 	Categories = {},
